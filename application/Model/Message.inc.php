68afbba3e2578685c4bb45c8ccd09051f09aab56
sam <sam@labelmedia.co.uk>
2012-07-28 15:52:58 +0100

    Adding start of some basic models

--- application/Model/Message.inc.php
+++ application/Model/Message.inc.php
@@ -8,13 +8,41 @@
 
 class Model_Message extends Model_MongoAbstract
 {
+	private 
+		$_text,
+		$_from;
+	
 	/**
-	 * Load a user by ID
+	 * Save the message
 	 * 
-	 * @return bool
+	 * @return LSF_Validation_Result
 	 */
-	public function load($id)
+	public function save()
 	{
-		return false;
+		$this->getDb()->message->insert($this->getData());
+		return new LSF_Validation_Result();
+	}
+	
+	/**
+	 * Returns data for storage
+	 * 
+	 * @return array
+	 */
+	private function getData()
+	{
+		return array('text' => $this->_text);
+	}
+	
+	/**
+	 * Set the message text
+	 * 
+	 * @param string $text
+	 * @return void
+	 */
+	public function setText($text)
+	{
+		if (is_string($text)) {
+			$this->_text = $text;
+		}
 	}
 }
