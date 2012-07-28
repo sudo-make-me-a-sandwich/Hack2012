40694cd4fa4d4610f41d5c0965d0c69860a80408
Martin Blackburn <walking_fishy@hotmail.com>
2012-07-28 23:23:49 +0100

    really adding new files this time :)

---
+++ application/Controller/Login.inc.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * /txt
+ * Login controller
+ *
+ * @package 
+ * $Id: Login.inc.php 6 2009-08-26 11:11:40Z sam $
+ */
+
+class Controller_Login extends LSF_Controller
+{
+	/**
+	 * Default index action
+	 * 
+	 * @return void
+	 */
+	protected function indexAction()
+	{
+		$form = new Form_Login();
+		$this->view->form = $form->render();
+		
+		if ($form->formSubmitted() && $form->formValidated())
+		{
+			/**
+			 * Temp code
+			 */
+			$user = new Model_User();
+			if (!$user->loadByPhonenumber($form->getElementValue('phonenumber'))) {
+				$this->redirect('register', null, null, '?phonenumber=' . $form->getElementValue('phonenumber'));
+			}
+			else
+			{
+				if ($user->auth($form->getElementValue('password')))
+				{
+					LSF_Session::GetSession()->phonenumber = $form->getElementValue('phonenumber');
+					LSF_Session::GetSession()->password = $user->hashPassword($form->getElementValue('password'));
+					$this->redirect('messages');
+				}
+				else {
+					$this->view->invalidLogin = true;
+				}
+			}
+		}
+	}
+}
