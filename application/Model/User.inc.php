2d24f1df82c67befdd641a27346565a983788464
sam <sam@labelmedia.co.uk>
2012-07-28 15:29:34 +0100

    Adding start of model classes

---
+++ application/Model/User.inc.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * User model
+ * 
+ * @author sam
+ */
+
+class Model_User extends Model_MongoAbstract
+{
+	/**
+	 * Load a user by ID
+	 * 
+	 * @return bool
+	 */
+	public function load($id)
+	{
+		return false;
+	}
+}
