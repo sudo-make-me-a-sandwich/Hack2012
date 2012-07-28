c6605e39e38c8abbbdce946ce0f4a2b5362f16d3
Tom Rawcliffe <tom@tomrawcliffe.o.uk>
2012-07-28 23:34:16 +0100

    Adding controller for incomming texts

---
+++ application/Controller/Incoming.inc.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * /txt
+ * Incoming controller
+ *
+ * @package 
+ * $Id: Default.inc.php 6 2009-08-26 11:11:40Z sam $
+ */
+
+class Controller_Incoming extends LSF_Controller
+{
+	/**
+	 * Default index action
+	 * 
+	 * @return void
+	 */
+	protected function indexAction()
+	{
+		$this->setPresenterName('none');
+		
+		$from = $this->getRequest()->getPostVar('from');
+		$text = $this->getRequest()->getPostVar('content');
+		
+		if (!empty($from) && !empty($text))
+		{
+			$incoming = new Model_Text_Incoming();
+			$incoming->setText($text);
+			$incoming->setFrom(Model_User::Get($from));
+			$incoming->process();
+		}
+	}
+}
